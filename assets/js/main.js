96778eecfb475f32cd8fbc1507bd1bcff671c403
mohammad jafa <jafamohammadgit@gmail.com>
2022-04-09 17:16:33 +0430

    add a logo and better reponsibility

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
   $("#bar").click(function () {
     $(".menu").toggleClass("active-canvas");
+    $(".main .main-overlay").toggleClass("main-overlay-active");
     $(".main").toggleClass("main-active");
     $(".canvas-menu").toggleClass("active-menu");
+  });
+  $(".main").click(function () {
+    $(".menu").removeClass("active-canvas");
+    $(".main .main-overlay").removeClass("main-overlay-active");
+    $(".main").removeClass("main-active");
+    $(".canvas-menu").removeClass("active-menu");
   });
 });
